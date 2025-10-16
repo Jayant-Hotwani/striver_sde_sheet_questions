c7b37f8f53a26bd8e080e2b72f6470d2e9e12cf6
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-10-16 23:29:56 +0530

    majority_element moore voting algorithm

---
+++ arrays/169_majority_element.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// moore voting algorithm
+// time complexity O(n) and space complexity O(1)
+// we have to find the element which is more than n/2 times in array
+// intution is if we take two different elements they will cancel each other
+// so in the end we will be left with the majority element
+// if we take two same elements they will increase the count of that element
+// so in the end we will be left with the majority element
+// if we take two different elements they will cancel each other 
+// so in the end we will be left with the majority element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        // very important moore voting algorithm
+
+        int n=nums.size();
+        int ele=0;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if (cnt==0){
+                ele=nums[i];
+            }
+            if(nums[i]!=ele){
+                cnt--;
+            }
+            else{
+                cnt++;
+            }
+            if (cnt==0){
+                cnt=0;
+            }
+        } 
+        return ele;
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3,2,3};
+    int result = sol.majorityElement(nums);
+    std::cout << "Majority Element: " << result << std::endl;
+    return 0;
+}
