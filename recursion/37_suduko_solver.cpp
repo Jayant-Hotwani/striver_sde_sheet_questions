17e2c1a62ce864da09eb3e5285b5081eb3ea3c5b
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-11-09 17:41:21 +0530

    suduko recursion_backtrackig

---
+++ recursion/37_suduko_solver.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    bool isValid(vector<vector<char>>& board,int row , int col , char c)
+    {
+        for (int i=0;i<9;i++)
+        {
+            if (board[i][col]==c) return false;
+            if (board[row][i]==c) return false;
+            if (board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+        
+    }
+
+    bool solve(vector<vector<char>>& board)
+    {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if (board[i][j]=='.')
+                {
+                    for(char ch='1';ch<='9';ch++){
+                        if (isValid(board,i,j,ch)){
+                            board[i][j]=ch;
+                            if (solve(board))return true;
+                            else
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<vector<char>> board(9,vector<char>(9));
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cin>>board[i][j];
+        }
+    }
+    sol.solveSudoku(board);
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
