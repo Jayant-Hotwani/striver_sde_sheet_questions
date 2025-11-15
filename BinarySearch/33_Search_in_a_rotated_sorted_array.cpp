39c10fc2885ca36eb59a207d1ce87208bfdf1708
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-11-15 11:57:50 +0530

    search in rotated sorted array

---
+++ BinarySearch/33_Search_in_a_rotated_sorted_array.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        // at any point of time 
+        // either of the lef half or right half would be sorted
+        // just check wheater our target lies in that range
+
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            if (nums[low]<=nums[mid]){
+                //left half sorted
+                if (target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                // right half sorted
+                if (target>=nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums={1,3,5,7,9,11,13};
+    int target=0;
+    cout<<sol.search(nums,target);
+    return 0;
+}
