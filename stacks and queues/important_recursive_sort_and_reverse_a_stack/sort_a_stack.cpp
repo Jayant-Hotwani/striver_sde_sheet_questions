6c0ee2313c52bfc42c199807f6be17bd162b63d7
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-11-27 16:27:10 +0530

    sort a stack without auxillary space

--- stacks and queues/important_recursive_sort_and_reverse_a_stack/sort_a_stack.cpp
+++ stacks and queues/important_recursive_sort_and_reverse_a_stack/sort_a_stack.cpp
@@ -1,26 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
   public:
-  
-    void insert_correct_position(stack<int>& st , int val){
+    
+    void insert_into_correct_position(stack<int>& st , int val){
         if (st.empty() || val>=st.top()){
             st.push(val);
         }
         else{
             int x=st.top();
             st.pop();
-            insert_correct_position(st,val);
+            insert_into_correct_position(st,val);
             st.push(x);
         }
-    } 
-  
+    }
     void sortStack(stack<int> &st) {
         // code here
-        if (st.size()==0) return ;
+        if (st.empty())return;
+        
+        // lets do it recusively
+        // assume rec func gives u sorted stack
+        // enter this element accordinly 
+        
         int x=st.top();
         st.pop();
         sortStack(st);
-        insert_correct_position(st,x);
+        
+        // after recursion ends 
+        // stack<int>temp;
+        // while(!st.empty()&&st.top()>x){
+        //     temp.push(st.top());
+        //     st.pop();
+        // }
+        // st.push(x);
+        // while(!temp.empty()){
+        //     st.push(temp.top());
+        //     temp.pop();
+        // }
+        
+        // we can do this without extra auxillary space too 
+        
+        insert_into_correct_position(st,x);
     }
 };
