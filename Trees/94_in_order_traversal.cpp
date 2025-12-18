c5e77a9a53bc3827135a880084e467fd50ce4e40
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-12-18 13:06:05 +0530

    iterative inorder

--- Trees/94_in_order_traversal.cpp
+++ Trees/94_in_order_traversal.cpp
@@ -22,6 +22,26 @@ public:
         vector<int>ans;
         inorder(root,ans);
         return ans;
+
+        // iterative approach
+        vector<int>inorder;
+        TreeNode* node=root;
+        stack<TreeNode*> st;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty())break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return inorder;
+        
         
     }
 };
