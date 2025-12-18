279701e7fb0cf2917b1a9a785a153b07fbb2c6b8
Jayant Hotwani <jayanthotwani2001@gmail.com>
2025-12-18 12:34:02 +0530

    iterative preOrder

--- Trees/144_preorder_traversal.cpp
+++ Trees/144_preorder_traversal.cpp
@@ -12,15 +12,30 @@ using namespace std;
  
 class Solution {
 public:
-void preorder(TreeNode* root , vector<int>& ans){
+    void preorder(TreeNode* root , vector<int>& ans){
         if (!root)return;
         ans.push_back(root->val);
         preorder(root->left,ans);
         preorder(root->right,ans);
     }
+
+
+
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>ans;
-        preorder(root,ans);
+        // preorder(root,ans);
+
+        // iterative approach
+        
+        stack<TreeNode*>st;
+        if(root) st.push(root);
+        while(!st.empty()){
+            TreeNode* node=st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
+        }
         return ans;
     }
 };
