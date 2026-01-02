9977455f15bd5cef2ad17803e6947046e8b5879f
Jayant Hotwani <jayanthotwani2001@gmail.com>
2026-01-02 14:35:48 +0530

    convert into bst

--- Trees/binary search tree/538_convert_bst_into_greater_tree.cpp
+++ Trees/binary search tree/538_convert_bst_into_greater_tree.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct TreeNode {
+class TreeNode {
+    public:
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}  
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+};  
 
 class Solution {
 public:
@@ -25,16 +25,33 @@ public:
         set_inorder(root->right,index,in);
     }
 
-
+    int sum=0;
     TreeNode* convertBST(TreeNode* root) {
-        vector<int>in;
-        inorder(root,in);
-        int n=in.size();
-        for(int i=n-2;i>=0;i--){
-            in[i]+=in[i+1];
-        }
-        int index=0;
-        set_inorder(root,index,in);
+
+        // naive approach
+
+        // vector<int>in;
+        // inorder(root,in);
+        // int n=in.size();
+        // for(int i=n-2;i>=0;i--){
+        //     in[i]+=in[i+1];
+        // }
+        // int index=0;
+        // set_inorder(root,index,in);
+        // return root;
+            // ---------------------------------------- // 
+
+        // start from right then root then left computing sum;
+
+        // optimal approach
+        if(!root) return root;
+
+        convertBST(root->right);
+        sum+=root->val;
+        root->val=sum;
+        convertBST(root->left);
+
         return root;
+
     }
 };
